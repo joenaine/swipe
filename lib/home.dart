47d3c4738895a1477a04b68e0c5c56703875cf51
joenaine <joenaine10@gmail.com>
2022-11-22 00:29:23 +0600

    [ADD] pageSwipe

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:swifeeapp/gest.dart';
+import 'package:swifeeapp/repos/list.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Home extends StatefulWidget {
@@ -13,88 +14,72 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   static ValueKey key = const ValueKey('key_0');
-  final PageController _listController = PageController();
+  final PageController _verticalController = PageController();
+
+  void navToPage(int index) {
+    _verticalController.animateToPage(index,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+  }
+
+  int pageNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       extendBodyBehindAppBar: true,
       floatingActionButton: FloatingActionButton(onPressed: () {
-        _listController.animateToPage(1,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.easeInOut);
+        if (pageNumber < webs.length) {
+          pageNumber++;
+        }
+        print(pageNumber);
+        navToPage(pageNumber);
       }),
-      body: PageView(
-        children: [
-          PageView(
-              controller: _listController,
-              scrollDirection: Axis.vertical,
-              physics: const NeverScrollableScrollPhysics(),
-              children: [
-                SizedBox(
-                  height: size.height,
-                  width: size.width,
-                  child: Center(
-                    child: WebView(
-                      initialUrl: 'https://flutter.dev/docs',
-                      gestureRecognizers: {
-                        Factory(() => PlatformViewVerticalGestureRecognizer(
-                              kind: PointerDeviceKind.touch,
-                            )),
-                      },
+      body: PageView.builder(
+        itemCount: webs.length,
+        itemBuilder: (context, index) {
+          return PageView.builder(
+            controller: _verticalController,
+            scrollDirection: Axis.vertical,
+            itemCount: webs.length,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, insideIndex) {
+              int pageSize = webs[index].length;
+              int navIndex = insideIndex;
+              int sizeOfWebs = 0;
+              return Stack(
+                children: [
+                  SizedBox(
+                    height: size.height,
+                    width: size.width,
+                    child: Center(
+                      child: WebView(
+                        initialUrl: webs[index][insideIndex],
+                        gestureRecognizers: {
+                          Factory(() => PlatformViewVerticalGestureRecognizer(
+                                kind: PointerDeviceKind.touch,
+                              )),
+                        },
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: size.height,
-                  width: size.width,
-                  child: Center(
-                    child: WebView(
-                      initialUrl: 'https://vk.com',
-                      gestureRecognizers: {
-                        Factory(() => PlatformViewVerticalGestureRecognizer(
-                              kind: PointerDeviceKind.touch,
-                            )),
-                      },
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height,
-                  width: size.width,
-                  child: Center(
-                    child: WebView(
-                      initialUrl: 'https://www.youtube.com/watch?v=RA-vLF_vnng',
-                      gestureRecognizers: {
-                        Factory(() => PlatformViewVerticalGestureRecognizer(
-                              kind: PointerDeviceKind.touch,
-                            )),
-                      },
-                    ),
-                  ),
-                ),
-              ]),
-          SizedBox(
-            height: size.height,
-            width: size.width,
-            child: const Center(
-              child: Text(
-                'Page 2',
-                style: TextStyle(fontSize: 72),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: size.height,
-            width: size.width,
-            child: const Center(
-              child: Text(
-                'Page 3',
-                style: TextStyle(fontSize: 72),
-              ),
-            ),
-          ),
-        ],
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (navIndex < webs[index].length - 1) {
+                            navIndex++;
+                          }
+                          print('index' '$navIndex');
+                          navToPage(navIndex);
+                        },
+                        child: const Text('data')),
+                  )
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
---
+++ lib/repos/list.dart
@@ -0,0 +1,15 @@
+List<List<String>> webs = [
+  [
+    'https://docs.flutter.dev/',
+    'https://jut.su',
+    'https://vk.com',
+  ],
+  [
+    'https://ru.wikipedia.org/wiki/Заглавная_страница',
+    'https://www.google.ru/',
+  ],
+  [
+    'https://www.facebook.com',
+    'https://jut.su',
+  ],
+];
